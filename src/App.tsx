829ffa9f33af6b43c4c69c4ed034311d9393d497
Jackson Miller <jacksonmiller7855@gmail.com>
2020-05-30 19:05:41 -0400

    Correct build and add SonarCloud

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import logo from "./logo.svg";
 import { Button } from "@material-ui/core";
 import RecipeBuilder from "./Components/RecipeBuilder";
 import Recipe from "./Core/Recipe";
-import Ingredient from "./Core/Ingredient";
 
 class App extends React.Component {
   recipe: Recipe;
@@ -13,15 +12,7 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <RecipeBuilder />
-      </div>
-    );
-  }
-
-  onButtonClick(params: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    this.recipe.addIngredient(new Ingredient("test"));
+    return <div></div>;
   }
 }
 
